6ba2f33f5fb3da85743cb37e3d636733bfec8a71
Nikola Smilyanski <96414322+nsmilyanski@users.noreply.github.com>
2022-05-21 12:22:53 +0300

    Dependency Injection exercises

---
+++ src/main/java/guru/springframework/sfgdi/controllers/Test.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi.controllers;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String s1 = "abc";
+        
+
+        String s2 = new String("abc").intern();
+
+        System.out.println();
+    }
+}
